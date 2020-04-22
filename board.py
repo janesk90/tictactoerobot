a470644f253e8db48ead0f2d25759d4cde044279
janesk90 <janesk90@gmail.com>
2020-04-21 23:27:05 -0400

    okay, so if you do like [[0]*3]*3 then all the lists in the list are the same, like referentially lmao

--- board.py
+++ board.py
@@ -4,15 +4,18 @@ O_VALUE = 2
 class Board(object):
     boardArr = None
     def __init__(self, size=3):
-        self.boardArr = [[EMPTY_SPACE]*size]*size
+        self.boardArr = []
+        for i in range(size):
+            self.boardArr.append([0]*size)
+    def _put(self, row, col, val):
+        self.boardArr[col][row] = val
     def putX(self, row, col):
-        self.boardArr[row][col] = X_VALUE
+        self._put(row, col, X_VALUE)
     def putO(self, row, col):
-        self.boardArr[row][col] = O_VALUE
+        self._put(row, col, O_VALUE)
     def toString(self):
         s = ""
         return str(self.boardArr)
 b = Board(3)
 b.putX(0,0)
 b.putO(0,1)
-print(b.toString())
